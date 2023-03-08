609cf399d7c3b0212771208b2c5585d47b36a5d0
Abhinay Pratap <abhinaypratap@icloud.com>
2023-03-08 07:24:35 +0530

    Setup SwiftLint

--- Messenger/ViewController.swift
+++ Messenger/ViewController.swift
@@ -1,19 +1,7 @@
-//
-//  ViewController.swift
-//  Messenger
-//
-//  Created by Abhinay Pratap on 08/03/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
