36d18311ee48f8f3e52b60826a5f7ad319df02d2
Abhinay Pratap <abhinaypratap@icloud.com>
2023-03-08 16:35:56 +0530

    Connect IBActions & IBOutlets

--- Messenger/Controller/ProfileViewController.swift
+++ Messenger/Controller/ProfileViewController.swift
@@ -1,8 +1,10 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 }
