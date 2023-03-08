edc704e364e250abf279d866776d0a78a9a4a30b
Abhinay Pratap <abhinaypratap@icloud.com>
2023-03-08 10:09:53 +0530

    Present suitable screen according on launch

--- Messenger/Controller/ConversationsViewController.swift
+++ Messenger/Controller/ConversationsViewController.swift
@@ -1,9 +1,25 @@
 import UIKit
 
-class ConversationsViewController: UIViewController {
+final class ConversationsViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Conversations"
-        view.backgroundColor = .darkGray
+        view.backgroundColor = .systemBackground
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        /// `bool(forKey:)`: The Boolean value associated with the specified key.
+        /// If the specified key doesn‘t exist, this method returns `false`.
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+
+        if !isLoggedIn {
+            let viewController = LoginViewController()
+            let navigationController = UINavigationController(rootViewController: viewController)
+            navigationController.modalPresentationStyle = .fullScreen
+            present(navigationController, animated: false)
+        }
     }
 }
