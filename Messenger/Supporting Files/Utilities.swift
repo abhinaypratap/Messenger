1b36e323c29e440aebb3dc402c088b6bf23232f0
Abhinay Pratap <abhinaypratap@icloud.com>
2023-03-08 07:54:14 +0530

    Extension for various sizes and positions of screens

---
+++ Messenger/Supporting Files/Utilities.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+extension UIView {
+
+    public var width: CGFloat {
+        frame.size.width
+    }
+
+    public var height: CGFloat {
+        frame.size.height
+    }
+
+    public var top: CGFloat {
+        frame.origin.y
+    }
+
+    public var bottom: CGFloat {
+        frame.size.height + frame.origin.y
+    }
+
+    public var left: CGFloat {
+        frame.origin.x
+    }
+
+    public var right: CGFloat {
+        frame.size.width + frame.origin.x
+    }
+}
